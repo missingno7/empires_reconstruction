4268d1b5358479b3fc27455b42d0e091e137a1a3
missingno7 <jiri.vestfal15@gmail.com>
2026-09-19 23:13:40 +0200

    Partition G43B4 record table BSS

--- tests/test_source_data_link.py
+++ tests/test_source_data_link.py
@@ -85,8 +85,8 @@ class SourceDataLinkTests(unittest.TestCase):
         self.assertTrue(report['byte_comparison']['load_image']['equal'])
         self.assertTrue(report['byte_comparison']['text']['equal'])
         self.assertEqual(report['synthetic_bss_bytes'], 0)
-        self.assertEqual(report['partitioned_bss_source_bytes'], 3588)
-        self.assertEqual(report['unpartitioned_bss_source_bytes'], 33662)
+        self.assertEqual(report['partitioned_bss_source_bytes'], 13588)
+        self.assertEqual(report['unpartitioned_bss_source_bytes'], 23662)
         self.assertFalse(report['dgroup_scaffold_present'])
         self.assertEqual(report['bss_source']['kind'], 'TASM_SOURCE_CONTRIBUTIONS')
         self.assertTrue(report['bss_source']['binding_evidence_equal'])
@@ -97,7 +97,8 @@ class SourceDataLinkTests(unittest.TestCase):
         self.assertEqual([(item['id'], item['bytes']) for item in report['bss_source']['contributions']],
                          [('F_01CE_BSS_PREFIX', 34), ('ROW_POINTER_TABLE_BSS', 1952),
                           ('RENDER_STATE_BSS', 16), ('RESOURCE25_WORKSPACE_BSS', 672),
-                          ('B4374_CONTROL_STATE_BSS', 64), ('GAMEBSS_LEADING_REMAINDER', 32610),
+                          ('B4374_CONTROL_STATE_BSS', 64), ('G43B4_RECORD_TABLE_BSS', 10000),
+                          ('GAMEBSS_LEADING_REMAINDER', 22610),
                           ('SLOT_GRID_BSS', 48), ('GAMEBSS_MID_REMAINDER', 8),
                           ('ANIMATION_STATE_BSS', 18), ('GC360_RECORD_TABLE_BSS', 270),
                           ('GAMEBSS_ERR_REMAINDER', 2),

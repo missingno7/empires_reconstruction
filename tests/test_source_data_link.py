34a1bc31f10128035b00dfa0d6cad2c5d6ac3fdf
missingno7 <jiri.vestfal15@gmail.com>
2026-09-20 00:22:21 +0200

    Partition F1D47 and F200F resource state BSS

--- tests/test_source_data_link.py
+++ tests/test_source_data_link.py
@@ -85,8 +85,8 @@ class SourceDataLinkTests(unittest.TestCase):
         self.assertTrue(report['byte_comparison']['load_image']['equal'])
         self.assertTrue(report['byte_comparison']['text']['equal'])
         self.assertEqual(report['synthetic_bss_bytes'], 0)
-        self.assertEqual(report['partitioned_bss_source_bytes'], 31602)
-        self.assertEqual(report['unpartitioned_bss_source_bytes'], 5648)
+        self.assertEqual(report['partitioned_bss_source_bytes'], 31938)
+        self.assertEqual(report['unpartitioned_bss_source_bytes'], 5312)
         self.assertFalse(report['dgroup_scaffold_present'])
         self.assertEqual(report['bss_source']['kind'], 'TASM_SOURCE_CONTRIBUTIONS')
         self.assertTrue(report['bss_source']['binding_evidence_equal'])
@@ -99,7 +99,8 @@ class SourceDataLinkTests(unittest.TestCase):
                           ('RENDER_STATE_BSS', 16), ('RESOURCE25_WORKSPACE_BSS', 672),
                           ('B4374_CONTROL_STATE_BSS', 64), ('G43B4_RECORD_TABLE_BSS', 10000),
                           ('F200F_RESOURCE_BUFFER_BSS', 1126), ('G6F2A_RECORD_TABLE_BSS', 904),
-                          ('A72B2_POINTER_TABLE_BSS', 160), ('GAMEBSS_RESOURCE_PREFIX', 336),
+                          ('A72B2_POINTER_TABLE_BSS', 160), ('F1D47_RESOURCE_POINTERS_BSS', 12),
+                          ('F200F_RESOURCE_44_45_BSS', 324),
                           ('A74A2_RECORD_TABLE_BSS', 1122), ('GAMEBSS_CEL_REGION', 4152),
                           ('G893C_RECORD_TABLE_BSS', 686), ('GAMEBSS_PRE_S8C12', 40),
                           ('S8C12_RESOURCE_BUFFER_BSS', 2770),

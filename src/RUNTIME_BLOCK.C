8481dd4ea6a42e1e48ca622f73beb3bbe47fe678
missingno7 <jiri.vestfal15@gmail.com>
2026-09-20 13:56:10 +0200

    Recover runtime nibble transform setup

--- src/RUNTIME_BLOCK.C
+++ src/RUNTIME_BLOCK.C
@@ -499,9 +499,32 @@ void f039c()
     asm shr bx,1
     asm mov dx,[bp+0ah]
     asm db 08bh,0c2h,048h,086h,0e0h,0d1h,0e8h,003h,0f8h,0d1h,0e8h,0d1h,0e8h,003h,0f8h,0b8h,0a0h,000h,08bh,0cbh,0f3h,0a4h,003h,0f0h,02bh
-    asm db 0f3h,02bh,0f8h,02bh,0fbh,04ah,075h,0f1h,01fh,05eh,05fh,05dh,0c3h,055h,08bh,0ech,057h,056h,01eh,0fch,08bh,05eh,00eh,0d1h
-    asm db 0e3h,0d1h,0e3h,0c4h,0bfh,024h,039h,08bh,046h,00ch,0d1h,0e8h,003h,0f8h,08bh,05eh,006h,0d1h,0e3h,0d1h,0e3h,0c5h,0b7h,024h
-    asm db 039h,08bh,046h,004h,0d1h,0e8h,003h,0f0h,08bh,05eh,008h,0d1h,0ebh,08bh,056h,00ah,003h,0fbh,04fh,08bh,0cbh,0ach,0d0h,0c0h
+    asm db 0f3h,02bh,0f8h,02bh,0fbh,04ah,075h,0f1h,01fh,05eh,05fh,05dh,0c3h
+    /* F_03BA: reverse-direction planar nibble transform. */
+    asm runtime_f03ba:
+    asm push bp
+    asm mov bp,sp
+    asm db 057h,056h
+    asm push ds
+    asm cld
+    asm mov bx,[bp+0eh]
+    asm shl bx,1
+    asm shl bx,1
+    asm db 0c4h,0bfh,024h,039h
+    asm mov ax,[bp+0ch]
+    asm shr ax,1
+    asm db 003h,0f8h
+    asm mov bx,[bp+6]
+    asm shl bx,1
+    asm shl bx,1
+    asm db 0c5h,0b7h,024h,039h
+    asm mov ax,[bp+4]
+    asm shr ax,1
+    asm db 003h,0f0h
+    asm mov bx,[bp+8]
+    asm shr bx,1
+    asm mov dx,[bp+0ah]
+    asm db 003h,0fbh,04fh,08bh,0cbh,0ach,0d0h,0c0h
     asm db 0d0h,0c0h,0d0h,0c0h,0d0h,0c0h,026h,088h,005h,04fh,0e2h,0f1h,081h,0c6h,0a0h,000h,02bh,0f3h,081h,0c7h,0a0h,000h,003h,0fbh
     asm db 04ah,075h,0e0h,01fh,05eh,05fh,05dh,0c3h,055h,08bh,0ech,057h,056h,01eh,0fch,08bh,05eh,00eh,0d1h,0e3h,0d1h,0e3h,0c4h,0bfh
     asm db 024h,039h,08bh,046h,00ch,0d1h,0e8h,003h,0f8h,08bh,05eh,006h,0d1h,0e3h,0d1h,0e3h,0c5h,0b7h,024h,039h,08bh,046h,004h,0d1h
